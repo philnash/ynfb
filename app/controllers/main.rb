e7de51431e236bcd57240e6fad95b5a5b9cbd366
philnash <philnash@gmail.com>
2008-12-14 17:09:10 +0000

    It fuckin' works

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,20 +1,16 @@
 class Main < Application
-  
   require 'scrobbler2'
-
   def index
     render
   end
-  
   def do_it
     @user = Scrobbler2::User.new(params[:username])
     @user.library
     @neighbours = @user.neighbours(:limit => 20)
     @all_artists = []
-      @neighbours.each do |n|
-        @all_artists << @neighbours.top_artists
-      end
-    Merb.logger.info("================hello===============")
+    @neighbours.each do |n|
+      @all_artists << n.top_artists
+    end
     @all_artists.flatten!
     @the_comparer = {}
     @all_artists.each do |a|
@@ -24,7 +20,26 @@ class Main < Application
         @the_comparer[a.name] = 1
       end
     end
-    render
+    Merb.logger.info(@user.library.to_s)
+    @the_comparer = @the_comparer.sort { |a,b| b[1]<=>a[1] }
+    library = @user.library.collect { |a| a.name }
+    @the_comparer.each do |k,v|
+      # puts  Scrobbler2::Artist.new(k)
+      unless library.include? k
+        @the_band = Scrobbler2::Artist.new(k)
+        break
+      end
+    end
+    render 
   end
-  
+end
+class Array
+  def include_name?(test)
+    result = false
+    self.each do |i|
+      if i.name == name
+        result = true
+      end
+    end
+  end
 end
